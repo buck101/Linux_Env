7fd19a65208725ce35965cf306d5330f2105ba34
buck <buck@51buck.(none)>
2015-02-05 03:19:22 +0000

    regular

---
+++ install.sh
@@ -0,0 +1,10 @@
+
+rm -rf ~/.gdbinit
+rm -rf ~/.vimrc
+rm -rf ~/.vim
+rm -rf ~/.bashrc
+
+ln -s `pwd`/bashrc ~/.bashrc
+ln -s `pwd`/gdbinit ~/.gdbinit
+ln -s `pwd`/vimrc ~/.vimrc
+ln -s `pwd`/vim ~/.vim
