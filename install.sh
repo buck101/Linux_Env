65f630fc818ce6c5daa70388aec284b0d433259e
buck101 <chenhui101@gmail.com>
2015-02-14 17:18:51 +0800

    regular

--- install.sh
+++ install.sh
@@ -6,5 +6,9 @@ rm -rf ~/.bashrc
 
 ln -s `pwd`/bashrc ~/.bashrc
 ln -s `pwd`/gdbinit ~/.gdbinit
+
 ln -s `pwd`/vimrc ~/.vimrc
+mkdir vim
 ln -s `pwd`/vim ~/.vim
+git submodule add https://github.com/tpope/vim-pathogen.git vim/bundle/vim-pathogen
+git submodule add https://github.com/mattn/emmet-vim.git vim/bundle/emmet-vim
