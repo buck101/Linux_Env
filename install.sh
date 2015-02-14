31545931f11fc640d5e72a7e9b0dbcf3789d174b
buck101 <chenhui101@gmail.com>
2015-02-14 17:40:33 +0800

    regulart

--- install.sh
+++ install.sh
@@ -12,3 +12,5 @@ mkdir vim
 ln -s `pwd`/vim ~/.vim
 git submodule add https://github.com/tpope/vim-pathogen.git vim/bundle/vim-pathogen
 git submodule add https://github.com/mattn/emmet-vim.git vim/bundle/emmet-vim
+git submodule add https://github.com/gmarik/vundle.git vim/bundle/vundle
+git submodule add https://github.com/scrooloose/nerdtree.git vim/bundle/nerdtree
