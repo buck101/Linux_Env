4b1c673302f8870e0d60949ee60a75097660cd58
buck101 <chenhui101@gmail.com>
2015-02-15 18:01:00 +0800

    regular

--- pack.sh
+++ pack.sh
@@ -1,6 +1,10 @@
 
 cd ..
 rm -rf Linux_Env.tar.Z
-#tar cvzf Linux_Env.tar.Z --exclude=.git ./Linux_Env/ 
-tar cvzf Linux_Env.tar.Z --exclude=.git --exclude=vim/bundle/YouCompleteMe ./Linux_Env/ 
+
+tar cvzf Linux_Env.tar.Z --exclude=.git --exclude=vim/bundle/YouCompleteMe ./Linux_Env/ > /dev/null 2>&1
+#tar cvzf Linux_Env.tar.Z --exclude=.git --exclude *.so --exclude *.exe --exclude *.dll --exclude *.pdb ./Linux_Env/ > /dev/null 2>&1
+
+#rm -rf Linux_Env.7z
+#7z a Linux_Env.7z ./Linux_Env -xr\!.git -xr\!*.so > /dev/null 2>&1
 cd -
